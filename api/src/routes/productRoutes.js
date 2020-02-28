5fbb3211ba7ebc99aea5866bc9223e3adbfd8b4f
Prabodh More <prabodh.more@cloudesign.in>
2020-02-28 07:33:09 +0530

    middleware added to product routes

--- api/src/routes/productRoutes.js
+++ api/src/routes/productRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import ProductController from '../controllers/productController';
 import ValidateProduct from '../middlewares/productValidator';
+import ValidateRequest from '../middlewares/auth';
 const router = Router();
 
 router.get('/', ProductController.getAllProducts);
 
-router.post('/', ValidateProduct, ProductController.addProduct);
+router.post('/', ValidateRequest, ValidateProduct, ProductController.addProduct);
 
-router.put('/:id', ValidateProduct, ProductController.updateProduct);
+router.put('/:id', ValidateRequest, ValidateProduct, ProductController.updateProduct);
 
-router.delete('/:id', ProductController.deleteProduct);
+router.delete('/:id', ValidateRequest, ProductController.deleteProduct);
 
 export default router;
